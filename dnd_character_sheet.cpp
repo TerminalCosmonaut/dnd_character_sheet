bb28b46b36d953fd11b47ea4d90acfb795516971
Blake Gilmore <bgi32@uclive.ac.nz>
2024-08-25 01:51:24 +1200

    User input working and storing values correctly

--- dnd_character_sheet.cpp
+++ dnd_character_sheet.cpp
@@ -7,16 +7,60 @@ g++ -o program dnd_character_sheet.cpp
 then run program.exe from within the directory
 */
 #include <iostream>
+#include <cstdint>
+#include <string> 
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+class dnd_class {
+    public:
+    int8_t something;
+    private:
+    int8_t otherthings;
+};
 
 class Character {
     public:
-    int8_t data;
+    void update_attributes () { //TODO: change newline chars to use endl
+      std::string strang;
+      cout << "Please enter attributes \n";
+      cout << "Charisma:\n";
+      cin >> strang;
+      cha = std::stoi(strang);
+      cout << "Wisdom:\n";
+      cin >> strang;
+      wis = std::stoi(strang);
+      cout << "Intelligence:\n";
+      cin >> strang;
+      intel = std::stoi(strang);
+      cout << "Dexterity:\n";
+      cin >> strang;
+      dex = std::stoi(strang);
+      cout << "Strength:\n";
+      cin >> strang;
+      str = std::stoi(strang);
+      cout << "Constitution:\n";
+      cin >> strang;
+      con = std::stoi(strang);
+    }
+
+    void print_attributes () { // TODO : make formatting nicer
+      cout << std::to_string(cha) << endl;
+      cout << std::to_string(wis) << endl;
+      cout << std::to_string(intel) << endl;
+      cout << std::to_string(dex) << endl;
+      cout << std::to_string(str) << endl;
+      cout << std::to_string(con) << endl;
+    }
     private:
     int8_t cha;
     int8_t wis;
     int8_t intel; // inteligence uses intel to not overwrite builtin in int type
-    int8_t wis;
+    int8_t dex;
     int8_t str;
+    int8_t con;
 
     bool multiclass; // maybe amalgamate
     dnd_class char_class; 
@@ -29,16 +73,13 @@ class Character {
 
 };
 
-class dnd_class {
-    public:
-    int8_t something;
-    private:
-    int8_t otherthings;
-};
+
 
 
 int main()
 {
-  std::cout << "Compiler Working Woohoo";
+  Character hulkenburg;
+  hulkenburg.update_attributes();
+  hulkenburg.print_attributes();
   return 0;
 }

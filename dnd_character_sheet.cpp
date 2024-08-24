4e3e215652429ba431dcb1827f2bcce6fe0483f6
Blake Gilmore <bgi32@uclive.ac.nz>
2024-08-24 23:30:16 +1200

    Initial Commit

---
+++ dnd_character_sheet.cpp
@@ -0,0 +1,44 @@
+/* A CLI based character sheet manager for 5th edition dnd 
+by Blake Gilmore 
+Started 24/08/24
+Version 1.0 
+Compile command so I don't forget
+g++ -o program dnd_character_sheet.cpp
+then run program.exe from within the directory
+*/
+#include <iostream>
+
+class Character {
+    public:
+    int8_t data;
+    private:
+    int8_t cha;
+    int8_t wis;
+    int8_t intel; // inteligence uses intel to not overwrite builtin in int type
+    int8_t wis;
+    int8_t str;
+
+    bool multiclass; // maybe amalgamate
+    dnd_class char_class; 
+
+    int8_t speed;
+    int16_t hp;
+
+
+    int8_t proficiency;
+
+};
+
+class dnd_class : private Character {
+    public:
+    int8_t something;
+    private:
+    int8_t otherthings;
+};
+
+
+int main()
+{
+  std::cout << "Compiler Working Woohoo";
+  return 0;
+}

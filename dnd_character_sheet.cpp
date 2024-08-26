cb09b2be3d33857132776f46f4dce66f59648ea5
Blake Gilmore <bgi32@uclive.ac.nz>
2024-08-26 18:12:14 +1200

    Got artificer sublcass selections working

--- dnd_character_sheet.cpp
+++ dnd_character_sheet.cpp
@@ -22,52 +22,55 @@ using std::find;
 
 class dnd_class {
     public:
-    bool pick_class () { 
+    void pick_class () { 
       std::string class_strang;
       cout << "Please select class" << endl;
       cout << "Artificer, Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock, Wizard" << endl;
       cin >> class_strang;
       bool in_vec = std::find(class_list_vec.begin(), class_list_vec.end(), class_strang) != class_list_vec.end();
-      if (in_vec == false ) {
+      while (in_vec == false ) {
         cerr << "Invalid class" << endl;
-        return true;
-      } else {
-        main_class = class_strang;
-        if (class_strang == "Artificer"){ // This is ugly but should only have to do it once
-          iterable_class = 0;
-        } if (class_strang == "Barbarian"){
-          iterable_class = 1;
-        } if (class_strang == "Bard"){
-          iterable_class = 2;
-        } if (class_strang == "Cleric"){
-          iterable_class = 3;
-        } if (class_strang == "Druid"){
-          iterable_class = 4;
-        } if (class_strang == "Fighter"){
-          iterable_class = 5;
-        } if (class_strang == "Monl"){
-          iterable_class = 6;
-        } if (class_strang == "Paladin"){
-          iterable_class = 7;
-        } if (class_strang == "Ranger"){
-          iterable_class = 8;
-        } if (class_strang == "Rogue"){
-          iterable_class = 9;
-        } if (class_strang == "Sorcerer"){
-          iterable_class = 10;
-        } if (class_strang == "Warlock"){
-          iterable_class = 11;
-        } if (class_strang == "Wizard"){
-          iterable_class = 12;
-        } 
-        return false;
+        cout << "Please select class" << endl;
+        cout << "Artificer, Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock, Wizard" << endl;
+        cin >> class_strang;
+        in_vec = std::find(class_list_vec.begin(), class_list_vec.end(), class_strang) != class_list_vec.end();
       }
-      return false;
-    }
+      main_class = class_strang;
+      if (class_strang == "Artificer"){ // This is ugly but should only have to do it once
+        iterable_class = 0;
+      } if (class_strang == "Barbarian"){
+        iterable_class = 1;
+      } if (class_strang == "Bard"){
+        iterable_class = 2;
+      } if (class_strang == "Cleric"){
+        iterable_class = 3;
+      } if (class_strang == "Druid"){
+        iterable_class = 4;
+      } if (class_strang == "Fighter"){
+        iterable_class = 5;
+      } if (class_strang == "Monl"){
+        iterable_class = 6;
+      } if (class_strang == "Paladin"){
+        iterable_class = 7;
+      } if (class_strang == "Ranger"){
+        iterable_class = 8;
+      } if (class_strang == "Rogue"){
+        iterable_class = 9;
+      } if (class_strang == "Sorcerer"){
+        iterable_class = 10;
+      } if (class_strang == "Warlock"){
+        iterable_class = 11;
+      } if (class_strang == "Wizard"){
+        iterable_class = 12;
+      }
+      return;
+      }
+      
     //generic method to pick a subclass for any class, automatically calls the class specific method
     void pick_sub_class_generic () { // TODO make various subclass pickers
       switch(iterable_class) {
         case 0:
+        pick_sub_class_artificer();
         break;
         case 1:
         break;
@@ -97,10 +100,31 @@ class dnd_class {
     }
     }
     private:
+    // picks a subclass for Artificer, private should only be called from pick_sub_class_generic or to be built level up and multiclass functions
+    void pick_sub_class_artificer () {
+      std::string sub_class_strang;
+      std::string cin_flushed;
+      cout << "Please select class" << endl;
+      cout << "Armorer, Alchemist, Artillerist, Battle Smith" << endl;
+      getline(cin,cin_flushed); // hacky way to clear previous main class input and avoid first input always erroring could be improved
+      getline(cin, sub_class_strang); 
+      bool in_vec = std::find(artificer_sublcasses_vec.begin(), artificer_sublcasses_vec.end(), sub_class_strang) != artificer_sublcasses_vec.end();
+      while (in_vec == false ) {
+        cerr << "Invalid class" << endl;
+        cout << "Please select class" << endl;
+        cout << "Armorer, Alchemist, Artillerist, Battle Smith" << endl;
+        getline(cin, sub_class_strang);
+        in_vec = std::find(artificer_sublcasses_vec.begin(), artificer_sublcasses_vec.end(), sub_class_strang) != artificer_sublcasses_vec.end();
+      }
+      sub_class = sub_class_strang;
+      return;
+    }
+    
     std::string main_class;
     std::string sub_class;
     int8_t iterable_class;
     std::vector<std::string> class_list_vec = {"Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
+    std::vector<std::string> artificer_sublcasses_vec = {"Armorer", "Alchemist", "Artillerist", "Battle Smith"};
     //int8_t iterable_vector[13] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
     bool multiclass;
 };
@@ -163,10 +187,7 @@ int main()
 {
   character hulkenburg;
   dnd_class haas;
-  bool err_check = 1;
-  while (err_check ==1) {
-    err_check = haas.pick_class(); 
-  }
+  haas.pick_class(); 
   haas.pick_sub_class_generic();
   cout << "Complete" << endl;
   //hulkenburg.update_attributes();
